33f8392ed7cd19bd6ef57105d61eb58c8d4ffe6d
aniketn <aniketn@umich.edu>
2024-08-04 02:28:07 -0400

    to-launch messed up - added the stepper component, almost have it done just need to figure out the routes and then it will be good. check clickable-stepper to see the routes logic, once thats done everything is set up. just need to edit the pages. the routes are all set too

--- app/app/(dashboard)/project/[columnId]/@development/[id]/page.tsx
+++ app/app/(dashboard)/project/[columnId]/@development/[id]/page.tsx
@@ -7,7 +7,7 @@ export default function DevelopmentRenderer({ params }: { params: {id: string} }
     console.log('[Path rending]', params)
     return (
         <>
-        <div>rendered</div>
+        <div>j</div>
         <ColumnRender columnId={params.id}/>
         </>
     )
