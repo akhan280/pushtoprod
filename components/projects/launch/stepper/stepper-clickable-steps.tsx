7ecbfa321b21c5be151da466f61e5c06ae2be0ed
Areeb Khan <areebk@umich.edu>
2024-08-06 15:39:26 -0700

    Cleaned up imports and button routing

--- components/projects/launch/stepper/stepper-clickable-steps.tsx
+++ components/projects/launch/stepper/stepper-clickable-steps.tsx
@@ -1,103 +1,111 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "../../../ui/button";
 import { StepItem } from "./types";
 import { Step, Stepper, useStepper } from ".";
 import useMainStore from "../../../../lib/hooks/use-main-store";
 
 const steps = [
-	{ label: "Start" },
-	{ label: "Finalize" },
-	{ label: "Deploy" },
-	{ label: "Market" },
+  { label: "Start" },
+  { label: "Finalize" },
+  { label: "Deploy" },
+  { label: "Market" },
 ] satisfies StepItem[];
 
 export default function StepperClickableSteps({
-	start,
-	deploy,
-	finalize,
-	market,
+  start,
+  deploy,
+  finalize,
+  market,
 }: {
-	start: React.ReactNode;
-	deploy: React.ReactNode;
-	finalize: React.ReactNode;
-	market: React.ReactNode;
+  start: React.ReactNode;
+  deploy: React.ReactNode;
+  finalize: React.ReactNode;
+  market: React.ReactNode;
 }) {
+  const { step, setLaunchStep, } = useMainStore();
+  const [currentStepContent, setCurrentStepContent] =
+    useState<React.ReactNode>(null);
 
-	const { step, setLaunchStep } = useMainStore();
-	const [currentStepContent, setCurrentStepContent] = useState<React.ReactNode>(null);
+  useEffect(() => {  console.log('step', step)  }, [step])
 
-	return (
-		<div className="flex w-full flex-col gap-4">
-			<Stepper
- 
-				initialStep={1}
-				steps={steps}
-				onClickStep={(step, setStep) => {
-					setStep(step);
-					setLaunchStep(step);
-				}}
-			>	
-				
-				<Step key={1} label={steps[0].label}>
-				{deploy}
-				</Step>
+  return (
+    <div className="flex w-full flex-col gap-4">
+      <Stepper
+        initialStep={step}
+        steps={steps}
+        onClickStep={(step, setStep) => {
+          setStep(step);
+          setLaunchStep(step);
+        }}
+      >
+        <Step label={steps[0].label}>
+          {start}
+        </Step>
 
-					<Step key={2} label={steps[2].label}>
-						{deploy}
-					</Step>
+        <Step key={1} label={steps[0].label}>
+          {deploy}
+        </Step>
 
-					<Step key={3} label={steps[3].label}>
-						{market}
-					</Step>
+        <Step key={2} label={steps[2].label}>
+          {deploy}
+        </Step>
 
-					<Footer />
-				</Stepper>
-			</div>
-		</div>
-	);
+        <Step key={3} label={steps[3].label}>
+          {market}
+        </Step>
+
+        <Footer />
+      </Stepper>
+    </div>
+  );
 }
 
 const Footer = () => {
-	const {
-		nextStep,
-		prevStep,
-		resetSteps,
-		isDisabledStep,
-		hasCompletedAllSteps,
-		isLastStep,
-		isOptionalStep,
-	} = useStepper();
+  const {
+    nextStep,
+    prevStep,
+    resetSteps,
+    isDisabledStep,
+    hasCompletedAllSteps,
+    isLastStep,
+    isOptionalStep,
+    activeStep
+  } = useStepper();
 
-	return (
-		<>
-			{hasCompletedAllSteps && (
-				<div className="h-40 flex items-center justify-center my-2 border bg-secondary text-primary rounded-md">
-					<h1 className="text-xl">Woohoo! All steps completed! 🎉</h1>
-				</div>
-			)}
-			<div className="w-full flex justify-end gap-2">
-				{hasCompletedAllSteps ? (
-					<Button size="sm" onClick={resetSteps}>
-						Reset
-					</Button>
-				) : (
-					<>
-						<Button
-							disabled={isDisabledStep}
-							onClick={prevStep}
-							size="sm"
-							variant="secondary"
-						>
-							Prev
-						</Button>
-						<Button size="sm" onClick={nextStep}>
-							{isLastStep ? "Finish" : isOptionalStep ? "Skip" : "Next"}
-						</Button>
-					</>
-				)}
-			</div>
-		</>
-	);
+  if (activeStep === 0) {
+    return(<div></div>)
+  }
+
+  return (
+    <>
+      {hasCompletedAllSteps && (
+        <div className="my-2 flex h-40 items-center justify-center rounded-md border bg-secondary text-primary">
+          <h1 className="text-xl">Woohoo! All steps completed! 🎉</h1>
+        </div>
+      )}
+      <div className="flex w-full justify-end gap-2">
+        {hasCompletedAllSteps ? (
+          <Button size="sm" onClick={resetSteps}>
+            Reset
+          </Button>
+        ) : (
+          <>
+            <Button
+              disabled={isDisabledStep}
+              onClick={prevStep}
+              size="sm"
+              variant="secondary"
+            >
+              Prev
+            </Button>
+            <Button size="sm" onClick={nextStep}>
+              {isLastStep ? "Finish" : isOptionalStep ? "Skip" : "Next"}
+            </Button>
+          </>
+        )}
+      </div>
+    </>
+  );
 };
