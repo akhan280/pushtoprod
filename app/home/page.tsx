e35d7759d8cf0a9a0e37b9b1f48b96c785f24565
Areeb Khan <areebk@umich.edu>
2024-08-07 02:37:21 -0700

    Further data added

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,4 @@
-import { Suspense } from "react";
-import Sites from "@/components/sites";
-import OverviewStats from "@/components/overview-stats";
-import Posts from "@/components/posts";
-import Link from "next/link";
-import PlaceholderCard from "@/components/placeholder-card";
-import OverviewSitesCTA from "@/components/overview-sites-cta";
+
 
 export default function Overview() {
   return (
@@ -36,7 +30,6 @@ export default function Overview() {
         </h1>
 
       </div>
-
     </div>
   );
 }
